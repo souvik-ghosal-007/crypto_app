de008157ee3052fc0f70d0368ca595e62a115ba9
souvik-ghosal-007 <souvik13.12.2000@gmail.com>
2022-10-15 16:04:51 +0530

    Fourth

--- app/src/main/java/com/souvik/cryptoapp/api/ApiInterface.kt
+++ app/src/main/java/com/souvik/cryptoapp/api/ApiInterface.kt
@@ -1,4 +1,11 @@
 package com.souvik.cryptoapp.api
 
+import com.souvik.cryptoapp.models.MarketModel
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface ApiInterface {
+
+    @GET("data-api/v3/cryptocurrency/listing?start=1&limit=500")
+    suspend fun getMarketData(): Response<MarketModel>
 }
